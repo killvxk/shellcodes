3989ed69bede14dc3ce939d0651627a549400e7c
Austin Hudson <austin.hudson@guidepointsecurity.com>
2019-12-01 00:33:54 -0500

    New utility

--- hash.c
+++ hash.c
@@ -23,15 +23,46 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#include <windows.h>
-#include "hs_util.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+unsigned long HashStringDjb2(const void *Input, unsigned long Length)
+{
+  const unsigned char *StringHash = (const unsigned char *)Input;
+  unsigned long        HashString = 5138;
+
+  while ( 1 ) {
+    char CurrentCharacter = *StringHash;
+
+    if (Length == 0) {
+      if (*StringHash == 0)
+          break;
+      } else {
+        if ((unsigned long)(StringHash - (const unsigned char *)Input) >= Length)
+          break;
+        if (*StringHash == 0) {
+          ++StringHash; continue;
+      };
+    };
+
+    if (CurrentCharacter >= 'a')
+      CurrentCharacter -= 0x20;
+
+    HashString  = ((HashString << 5) + HashString) + CurrentCharacter;
+
+    ++StringHash;
+  }
+  return HashString;
+};
 
 int main(int argc, char *argv[]) {
-	if ( argc < 2 ) {
-		printf("usage: %s [string]\n", argv[0]);
-		ExitProcess(0);
-	};
-	DWORD HshStr = HashStringDjb2(argv[1], strlen(argv[1]));
-	printf("Hash 0x%x\n", HshStr);
+  if ( argc < 2 ) {
+    printf("usage: %s [string]\n", argv[0]);
+    exit(0);
+  };
+
+  printf("Str: %s Hash: 0x%lx\n", argv[1],
+    HashStringDjb2(argv[1], strlen(argv[1])));
 };
